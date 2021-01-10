372599a56296597ab71b2d1e263749528bc04723
Monique DeSalvo <moniquerenee@gmail.com>
2021-01-10 11:20:15 -0800

    Format daily doses & date

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+const addCommasToBigNumber = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 const run = async () => {
   let data = await d3.csv("cdph-data.csv", d3.autoType);
+  window.data = data;
+  const totalSortedByDate = data
+    .filter((row) => row.region === "Total")
+    .sort((a, b) => a.date - b.date);
+
+  const latestTotalDosesRow = totalSortedByDate[totalSortedByDate.length - 1];
+
+  let totalDosesAdministered = latestTotalDosesRow.total_doses_administered;
+  const totalFromDayBefore =
+    totalSortedByDate[totalSortedByDate.length - 2].total_doses_administered;
+
+  const dailyDoseTotal = totalDosesAdministered - totalFromDayBefore;
+
+  totalDosesAdministered = addCommasToBigNumber(totalDosesAdministered);
+
+  const recentTotalDate = latestTotalDosesRow.date;
+
+  const formattedRecentTotalDate = d3.timeFormat("%B %e, %Y")(recentTotalDate);
+
+  document.querySelector("#total-doses").innerText = totalDosesAdministered;
+
+  document.querySelector("#date").innerText = formattedRecentTotalDate;
+
+  document.querySelector("#daily-doses").innerText = addCommasToBigNumber(
+    dailyDoseTotal
+  );
+
   data = data.filter((row) => row.region === "Total");
 
   console.log(data);
