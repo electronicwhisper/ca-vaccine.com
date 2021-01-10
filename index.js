1fb27f7264197c9512da915e727a6de1945cb58f
Monique DeSalvo <moniquerenee@gmail.com>
2021-01-10 13:17:39 -0800

    Switched to using plotly library

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const addCommasToBigNumber = (num) => {
 
 const run = async () => {
   let data = await d3.csv("cdph-data.csv", d3.autoType);
-  window.data = data;
   const totalSortedByDate = data
     .filter((row) => row.region === "Total")
     .sort((a, b) => a.date - b.date);
@@ -35,73 +34,38 @@ const run = async () => {
 
   console.log(data);
 
-  const width = 800;
-  const height = 500;
+  window.data = data;
 
-  const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+  const x = data.map((row) => d3.timeFormat("%Y-%m-%d")(row.date));
 
-  const margin = { top: 20, right: 30, bottom: 30, left: 60 };
+  const y = data.map((row) => row.total_doses_administered);
 
-  const x = d3
-    .scaleUtc()
-    .domain(d3.extent(data, (d) => d.date))
-    .range([margin.left, width - margin.right]);
+  window.x = x;
 
-  const timeFormat = d3.utcFormat("%b %e");
-
-  const xAxis = (g) =>
-    g
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(
-        d3
-          .axisBottom(x)
-          .ticks(d3.utcDay)
-          .tickFormat(timeFormat)
-          .tickSizeOuter(0)
-      );
-
-  svg.append("g").call(xAxis);
-
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.total_doses_administered)])
-    .nice()
-    .range([height - margin.bottom, margin.top]);
-
-  const yAxis = (g) =>
-    g
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y))
-      .call((g) => g.select(".domain").remove())
-      .call((g) =>
-        g
-          .select(".tick:last-of-type text")
-          .clone()
-          .attr("x", 3)
-          .attr("text-anchor", "start")
-          .attr("font-weight", "bold")
-          .text("Total Doses Administered")
-      );
-
-  svg.append("g").call(yAxis);
-
-  const line = d3
-    .line()
-    .defined((d) => !isNaN(d.total_doses_administered))
-    .x((d) => x(d.date))
-    .y((d) => y(d.total_doses_administered));
-
-  svg
-    .append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("d", line);
-
-  document.body.appendChild(svg.node());
+  Plotly.newPlot(
+    "chart",
+    [{ x: x, y: y, type: "scatter" }],
+    {
+      title: {
+        text: "Total Vaccine Doses Administered in California",
+        font: {
+          size: 20,
+        },
+        xref: "paper",
+        x: 0.05,
+      },
+      yaxis: {
+        tickmode: "linear",
+        tick0: 0,
+        dtick: 100000,
+        rangemode: "tozero",
+        autorange: true,
+      },
+    },
+    {
+      displayModeBar: false,
+    }
+  );
 };
 
 run();
